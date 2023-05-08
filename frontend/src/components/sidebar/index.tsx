125e3deda7b1df1ddcb2a2a4bd2951d32e2532da
DmitriyKulikovskiy23 <dmitriy.kulikovskiy23@gmail.com>
2023-05-08 21:04:31 +0300

    FM-2

--- frontend/src/components/sidebar/index.tsx
+++ frontend/src/components/sidebar/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { HiHome, HiMail } from "react-icons/hi";
+import { GiDarkSquad } from "react-icons/gi";
+import { AiOutlineForm, AiOutlineSchedule } from "react-icons/ai";
+import { BsConeStriped, BsShieldShaded } from "react-icons/bs";
+import { RiMoneyDollarBoxFill } from "react-icons/ri";
 
 import "./styles/index.scss";
 
@@ -7,15 +12,44 @@ const SideBar = () => {
     return (
         <div className="sidebar">
             <ul className="sidebar_list">
-                <li className="sidebar_item">
-                    <Link to="/homepage">Homepage</Link>
-                    <Link to="/inbox">Inbox</Link>
-                    <Link to="/squad">Squad</Link>
-                    <Link to="/tactic">Tactic</Link>
-                    <Link to="/trainings">Training</Link>
-                    <Link to="/schedule">Schedule</Link>
-                </li>
+                <NavLink to="/homepage" className="sidebar_item">
+                    <HiHome />
+                    <span>Homepage</span>
+                </NavLink>
+                <NavLink to="/inbox" className="sidebar_item">
+                    <HiMail />
+                    <span>Inbox</span>
+                </NavLink>
+                <NavLink to="/squad" className="sidebar_item">
+                    <GiDarkSquad />
+                    <span>Squad</span>
+                </NavLink>
+                <NavLink to="/tactic" className="sidebar_item">
+                    <AiOutlineForm />
+                    <span>Tactic</span>
+                </NavLink>
+                <NavLink to="/trainings" className="sidebar_item">
+                    <BsConeStriped />
+                    <span>Training</span>
+                </NavLink>
+                <NavLink to="/schedule" className="sidebar_item">
+                    <AiOutlineSchedule />
+                    <span>Schedule</span>
+                </NavLink>
+                <NavLink to="/club" className="sidebar_item">
+                    <BsShieldShaded />
+                    <span>Club</span>
+                </NavLink>
+                <NavLink to="/finance" className="sidebar_item">
+                    <RiMoneyDollarBoxFill />
+                    <span>Finance</span>
+                </NavLink>
             </ul>
+
+            <div className="sidebar_bottom">
+                <div className="sidebar_logo">IManager</div>
+                <div className="sidebar_version">Version 0.01</div>
+            </div>
         </div>
     )
 }
